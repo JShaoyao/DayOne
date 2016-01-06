9d971ab419f40f36f1a0bf53c7cc55294a0066bd
JShaoyao <1558390342@qq.com>
2016-01-06 11:15:35 +0800

    撤销操作try
    撤销

--- Collision.cpp
+++ Collision.cpp
@@ -32,17 +32,17 @@ static bool isCollision(Sprite*sp1,Sprite*sp2)
     //    }
     //    return false;
 }
-static bool iscollision2(Sprite*sp1,Sprite*sp2)
-{
-    //自定义碰撞
-    Point p1=sp1->getPosition();
-    Point p2=sp2->getPosition();
-    Size p1s=sp1->boundingBox().size;
-    Size p2s=sp2->boundingBox().size;
-    //根据坐标和精灵大小来检测碰撞
-    if((fabs(p1.x-p2.x)<=(p1s.width/2+p2s.width/2-20))&&(fabs(p1.y-p2.y)<=(p1s.height/2+p2s.height/2-20)))
-    {
-        return true;
-    }
-    return false;
-}
+// static bool iscollision2(Sprite*sp1,Sprite*sp2)
+// {
+//     //自定义碰撞
+//     Point p1=sp1->getPosition();
+//     Point p2=sp2->getPosition();
+//     Size p1s=sp1->boundingBox().size;
+//     Size p2s=sp2->boundingBox().size;
+//     //根据坐标和精灵大小来检测碰撞
+//     if((fabs(p1.x-p2.x)<=(p1s.width/2+p2s.width/2-20))&&(fabs(p1.y-p2.y)<=(p1s.height/2+p2s.height/2-20)))
+//     {
+//         return true;
+//     }
+//     return false;
+// }
